db0ff26b5918ecd8c0fd3994763008cc0a93260b
Thiago Coppi <coppithiago@gmail.com>
2017-08-24 17:31:40 -0300

    Criação dos includes de créditos/débitos

--- frontend/app/billingCycle/billingCycleController.js
+++ frontend/app/billingCycle/billingCycleController.js
@@ -12,7 +12,8 @@
             const url = 'http://localhost:3003/api/billingCycles'
             vm.refresh = function(){                
                 $http.get(url).then(function(response){
-                    vm.billingCycle = {}
+                    vm.billingCycle = {credits: [{}], debts: [{}]}
+                    vm.calculateValues()
                     vm.billingCycles = response.data
                     tabs.show(vm,{tabList: true, tabCreate : true})
                 })
@@ -50,12 +51,62 @@
 
             vm.showTabDelete = function(billingCycle){
                 vm.billingCycle = billingCycle
+                vm.calculateValues()
                 tabs.show(vm,{tabDelete: true})
             }
 
             vm.showTabUpdate = function(billingCycle){
                 vm.billingCycle = billingCycle
+                vm.calculateValues()
                 tabs.show(vm,{tabUpdate: true})
+            }
+
+            vm.addCredit = function(index){
+                vm.billingCycle.credits.splice(index + 1,0,{})
+                vm.calculateValues()
+            }
+
+            vm.cloneCredit = function(index,{name,value}){
+                vm.billingCycle.credits.splice(index + 1,0,{name,value})  
+                vm.calculateValues()              
+            }
+
+            vm.deleteCredit = function(index){
+                if(vm.billingCycle.credits.length > 1)
+                    vm.billingCycle.credits.splice(index,1)
+                vm.calculateValues()
+            }
+
+            vm.addDebt = function(index){
+                vm.billingCycle.debts.splice(index + 1,0,{})
+                vm.calculateValues()
+            }
+
+            vm.cloneDebt = function(index,{name,value,status}){
+                vm.billingCycle.debts.splice(index + 1,0,{name,value,status})
+                vm.calculateValues()
+            }
+
+            vm.deleteDebt = function(index){
+                if(vm.billingCycle.debts.length > 1)
+                    vm.billingCycle.debts.splice(index,1)
+                vm.calculateValues()
+            }
+
+            vm.calculateValues = function(){
+                vm.credit = 0
+                vm.debt = 0
+
+                if(vm.billingCycle){
+                    vm.billingCycle.credits.forEach(function({value}){                        
+                        vm.credit+= !value || isNaN(value) ? 0 : parseFloat(value)
+                    })
+
+                    vm.billingCycle.debts.forEach(function({value}){                        
+                        vm.debt+= !value || isNaN(value) ? 0 : parseFloat(value)
+                    })
+                }                 
+                vm.total = vm.credit - vm.debt                
             }
 
             vm.refresh()
